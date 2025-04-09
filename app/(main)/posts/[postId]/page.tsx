708ac5fa9ab5ff71549b3ffedb83c6685fa0fd53
immanuelk1m <kse0119@naver.com>
2025-04-09 23:38:13 +0900

    ignore params any

--- app/(main)/posts/[postId]/page.tsx
+++ app/(main)/posts/[postId]/page.tsx
@@ -3,7 +3,8 @@ import PostDetailClient from './PostDetailClient';
 import { getPostById } from '@/services/post.service';
 import type { PostWithDetails } from '@/services/post.service';
 
-// 타입 정의를 완전히 생략하고 단순화된 방식 사용
+// ESLint 경고 비활성화
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async function PostDetailPage({ params }: any) {
   const postId = params.postId as string;
   const post: PostWithDetails | null = await getPostById(postId);
